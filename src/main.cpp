b72998a60713dd590c844fc6e3fc26bdc8c4b823
seryafarma <enhope.estheim@gmail.com>
2023-02-11 15:25:55 +0100

    First working version.

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,16 @@
 #include <Arduino.h>
 
 #include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <MD_MAX72xx.h>
 #include <MD_Parola.h>
 #include <SPI.h>
 #include <StateMachine.h>
+
+#include <WiFiClient.h>
+
+WiFiClient wifiClient;
 
 //---------------------------------------------------------------------------------------------------------------------
 // Local Includes
@@ -35,6 +40,121 @@
 #define STATE_DELAY_MS 100
 
 //---------------------------------------------------------------------------------------------------------------------
+// Class
+//---------------------------------------------------------------------------------------------------------------------
+
+struct WeatherInfo
+{
+    String name;
+    String weather;
+    String weather_desc;
+    String temperature_c;
+    String humidity_perc;
+    String counter;
+
+    void clear()
+    {
+        weather = weather_desc = temperature_c = humidity_perc = "";
+    }
+
+    String get_string()
+    {
+        String n = "Name: " + name + ", Weather: " + weather + ", Weather_Desc: " + weather_desc +
+                   ", Temperature: " + temperature_c + ", Humid: " + humidity_perc + " = [Counter " + counter + "]";
+        return n;
+    }
+};
+
+class WeatherReader
+{
+public:
+    WeatherReader(String properties)
+    {
+        url = "http://api.openweathermap.org/data/2.5/weather?" + properties;
+    }
+
+    void read()
+    {
+        get_http_weather(url.c_str());
+    }
+
+    WeatherInfo get_current_weather()
+    {
+        return current_weather;
+    }
+
+private:
+    void get_http_weather(const char* serverName)
+    {
+        static int mycounter = 0;
+
+        current_weather.clear();
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            String json_array = get_request(serverName);
+
+            Serial.println(json_array);
+
+            DynamicJsonDocument doc(1024);
+            deserializeJson(doc, json_array);
+
+            {
+                const char* weather = NULL;
+                const char* weather_desc = NULL;
+                const char* name = NULL;
+                double temp = 0.0;
+                long hum = 0;
+
+                weather = doc["weather"][0]["main"];
+                current_weather.weather = String(weather);
+                weather_desc = doc["weather"][0]["description"];
+                current_weather.weather_desc = String(weather_desc);
+
+                temp = doc["main"]["temp"];
+                temp -= 273.15;
+                current_weather.temperature_c = String(temp);
+
+                hum = doc["main"]["humidity"];
+                current_weather.humidity_perc = String(hum);
+
+                name = doc["name"];
+                current_weather.name = String(name);
+
+                mycounter++;
+                current_weather.counter = String(mycounter);
+            }
+        }
+    }
+
+    String get_request(const char* server)
+    {
+        HTTPClient http;
+        http.begin(wifiClient, server);
+        int httpResponseCode = http.GET();
+
+        String payload = "{}";
+
+        if (httpResponseCode > 0)
+        {
+            Serial.print("HTTP Response code: ");
+            Serial.println(httpResponseCode);
+            payload = http.getString();
+        }
+        else
+        {
+            Serial.print("Error code: ");
+            Serial.println(httpResponseCode);
+        }
+        http.end();
+
+        return payload;
+    }
+
+    String url;
+    WeatherInfo current_weather;
+};
+
+//---------------------------------------------------------------------------------------------------------------------
 // Global Variables
 //---------------------------------------------------------------------------------------------------------------------
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
@@ -43,6 +163,9 @@ String things_to_show = "0";
 uint32_t dummy_counter = 0;
 
 StateMachine machine = StateMachine();
+
+WeatherInfo weather_info;
+WeatherReader wr(Authentication::API_KEY);
 
 uint32_t previous_millis = 0;
 //---------------------------------------------------------------------------------------------------------------------
@@ -56,15 +179,33 @@ bool ev_idle();
 //---------------------------------------------------------------------------------------------------------------------
 // Setup and Loop
 //---------------------------------------------------------------------------------------------------------------------
+void connectWifi()
+{
+    WiFi.begin(Authentication::WIFI_SSID, Authentication::WIFI_PASSWORD);
+    Serial.print("Connecting to ");
+    Serial.println(Authentication::WIFI_SSID);
 
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected!");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+}
 //---------------------------------------------------------------------------------------------------------------------
 void setup()
 {
     Serial.begin(9600);
     P.begin();
+    delay(1500);
+    connectWifi();
 
-    State* idle = machine.addState(&idle_state);
     State* gather = machine.addState(&gather_state);
+    State* idle = machine.addState(&idle_state);
     idle->addTransition(&ev_gather, gather);
     gather->addTransition(&ev_idle, idle);
 }
@@ -120,7 +261,10 @@ void gather_state()
         Serial.println("[Gather State, Entering]");
         // Dummy
         dummy_counter += 1;
-        things_to_show = String(dummy_counter);
+        wr.read();
+        weather_info = wr.get_current_weather();
+
+        things_to_show = String(weather_info.get_string());
     }
 }
 
